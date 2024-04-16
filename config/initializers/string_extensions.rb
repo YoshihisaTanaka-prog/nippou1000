a1b57fed610bbbc33f0c675130b039b1299084a2
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2024-04-16 12:32:47 +0900

    Update string_extensions.rb

--- config/initializers/string_extensions.rb
+++ config/initializers/string_extensions.rb
@@ -4,6 +4,7 @@ class String
     from_array.each do |from|
       modified_text = modified_text.gsub(from, to_first_person)
     end
+    modified_text = modified_text.gsub("今日", "本日")
     return modified_text
   end
 end
