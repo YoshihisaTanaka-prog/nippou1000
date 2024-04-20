8c893a0534519c5bce4b8bb71ac94fc19c1e8266
YoshihisaTanaka-prog <nakanaka.tanaka4413@gmail.com>
2024-04-20 13:06:44 +0900

    Update 20240419224600_add_user_to_is_shareable_zoho.rb

--- db/migrate/20240419224600_add_user_to_is_shareable_zoho.rb
+++ db/migrate/20240419224600_add_user_to_is_shareable_zoho.rb
@@ -1,5 +1,6 @@
 class AddUserToIsShareableZoho < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :is_shareable_zoho, :boolean
+    add_column :users, :is_shared_zoho, :boolean
   end
 end
